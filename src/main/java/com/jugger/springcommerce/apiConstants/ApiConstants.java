ebdc08a832311bb3293fd1abcb805ca00fd254ce
rahul161616 <rahulsewa1616@gmail.com>
2026-03-26 19:42:59 +0545

    Initialize Spring Boot project with Gradle, basic configurations, and API constants.

---
+++ src/main/java/com/jugger/springcommerce/apiConstants/ApiConstants.java
@@ -0,0 +1,7 @@
+package com.jugger.springcommerce.apiConstants;
+
+public class ApiConstants {
+    private static final String BASE_URL = "/api/v1";
+    public static final String ADMIN_PRODUCT_API = BASE_URL + "/admin/products";
+    public static final String PRODUCT_PRODUCT_API = BASE_URL + "/public";
+}
