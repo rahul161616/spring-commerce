cb527fb9794f36be36a928898feba7282840112d
rahul161616 <rahulsewa1616@gmail.com>
2026-03-31 03:11:12 +0545

    feat: Add public homepage controller and response DTO structure

---
+++ src/main/java/com/jugger/springcommerce/modules/homepage/controller/HomepagePublicController.java
@@ -0,0 +1,16 @@
+package com.jugger.springcommerce.modules.homepage.controller;
+
+import com.jugger.springcommerce.apiConstants.ApiConstants;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(ApiConstants.HOMEPAGE_PUBLIC)
+public class HomepagePublicController {
+
+//    @GetMapping
+//    public ResponseEntity<HomepagePublicResponse>getHomePageResponse(){
+//    }
+}
