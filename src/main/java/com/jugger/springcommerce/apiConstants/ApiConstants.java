4c1a37a777b1e50e9b04ad720a3745fcf0b9e1c9
rahul161616 <rahulsewa1616@gmail.com>
2026-03-31 16:48:57 +0545

    feat: Add public product methods with mappers, services, and controller integration for public endpoint

--- src/main/java/com/jugger/springcommerce/apiConstants/ApiConstants.java
+++ src/main/java/com/jugger/springcommerce/apiConstants/ApiConstants.java
@@ -3,7 +3,7 @@ package com.jugger.springcommerce.apiConstants;
 public class ApiConstants {
     private static final String BASE_URL = "/api/v1";
     public static final String ADMIN_PRODUCT_API = BASE_URL + "/admin/products";
-    public static final String PRODUCT_PRODUCT_API = BASE_URL + "/public";
+    public static final String PUBLIC_PRODUCT_API = BASE_URL + "/public/products";
     public static final String ADMIN_CATEGORIES_API = BASE_URL + "/admin/categories";
     public static final String PUBLIC_CATEGORIES_API = BASE_URL + "/public/categories";
     public static final String ADMIN_TAGS_API = BASE_URL + "/admin/tags";
